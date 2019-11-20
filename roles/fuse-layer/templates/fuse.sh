664da9f12520db38b9de6bf03e098fc441f84444
marieke-bijlsma <mariekebijlsma01@gmail.com>
2019-11-20 11:15:28 +0100

    Update ega-fuse-client role

--- roles/fuse-layer/templates/fuse.sh
+++
@@ -1,8 +0,0 @@
-source "/apps/modules//modules.bashrc"
-module load Java/8-LTS
-
-java -Xmx2g -jar /usr/local/fuse-layer/ega-fuse-1.0-SNAPSHOT.jar \
--f /usr/local/fuse-layer/config.ini \
--m "{{ fuse_mountpoint }}" \
--u "{{ fuse_user }}" \
--p "{{ fuse_password }}"
