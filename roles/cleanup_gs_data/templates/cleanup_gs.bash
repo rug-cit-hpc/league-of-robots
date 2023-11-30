41f30d38412139d4c01216affda90c8ddf3c783a
marieke-bijlsma <mariekebijlsma01@gmail.com>
2023-11-30 11:17:24 +0100

    Add sending slack notification to cleanup script and change weekly cronjob to daily cronjob

--- roles/cleanup_gs_data/templates/cleanup_gs.bash
+++ roles/cleanup_gs_data/templates/cleanup_gs.bash
@@ -3,15 +3,13 @@
 # If folder is not empty and no .finished file is present (1 week) -> notify helpdesk
 # If folder is not empty and no .finished file is present (2 weeks) -> delete folder
 
-#TODO: notifications and removal of folders
-
 #!/bin/bash
 
 dirToCheck="/groups/umcg-genomescan/"*
 dateInSecNow=$(date +%s)
 
 # Check only dirs, ignore files
-for dir in $(find "${dirToCheck}" -mindepth 1 -maxdepth 1 -type d)
+for dir in $(find ${dirToCheck} -mindepth 1 -maxdepth 1 -type d)
 do
 	# Check if dir contains data
 	if [[ $(ls -A "${dir}")  ]]
@@ -24,14 +22,34 @@ do
 			echo ".finished file found, processing of data should start soon"
 		fi
 	else
-		echo "${dir} is empty, check if it's older than 1 week -> mail"
 		if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
 		then
 			echo "${dir} is older than 14 days and will be deleted"
-			#rm -rf "${dir}"
+			rm -rf "${dir}"
 		elif [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 7 ]]
 		then
 			echo "${dir} is older than 7 days, notification will be send"
+			
+			#
+			# Compile JSON message payload.
+			#
+			read -r -d '' message << EOM
+{
+	"type": "mrkdwn",
+	"text": "*Cleanup alert on _{{ slurm_cluster_name | capitalize }}_*:  
+\`\`\`
+The following data on $(hostname) of the {{ slurm_cluster_name | capitalize }} cluster is older than a week: ${dir}. This data will be deleted when it's older than two weeks!
+\`\`\`"
+}
+EOM
+
+#
+# Post message to Slack channel.
+#
+curl -X POST '{{ slurm_notification_slack_webhook }}' \
+	 -H 'Content-Type: application/json' \
+	 -d "${message}"
+					
 		else
 			echo "${dir} is not older than 7 days"
 		fi
