2f47bbecc8de2628ae3784ba24ca68c430e1f262
marieke-bijlsma <mariekebijlsma01@gmail.com>
2023-12-13 19:47:58 +0100

    Changes to gs cleanup script (WIP)

--- roles/cleanup_gs_data/templates/cleanup_gs.bash
+++ roles/cleanup_gs_data/templates/cleanup_gs.bash
@@ -1,7 +1,6 @@
-# If folder is empty for 1 week -> notify helpdesk
 # If folder is empty for 2 weeks -> delete folder
-# If folder is not empty and no .finished file is present (1 week) -> notify helpdesk
-# If folder is not empty and no .finished file is present (2 weeks) -> delete folder
+# If folder is not empty and no .finished file is present (1 week) -> notification
+# If folder is not empty and .finished file is present (2 weeks) -> delete folder
 
 #!/bin/bash
 
@@ -11,51 +10,55 @@ dateInSecNow=$(date +%s)
 # Check only dirs, ignore files
 for dir in $(find ${dirToCheck} -mindepth 1 -maxdepth 1 -type d)
 do
-	# Check if dir contains data
-	if [[ $(ls -A "${dir}")  ]]
+	if [[ ! $(ls -A "${dir}") ]]
 	then
-		echo "There is Data in ${dir}, check if .finished file is present"
-		if [[ ! "${dir}/"*".finished" ]]
-		then
-			echo "No .finished file found, but ${dir} is not empty"
-		else
-			echo ".finished file found, processing of data should start soon"
+		echo "${dir} is empty, check if it's older than 2 week -> delete"
+		if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
+                then
+                        echo "${dir} is older than 14 days and will be deleted"
+                        rm -rf "${dir}"
 		fi
 	else
-		echo "${dir} is empty, check if it's older than 1 week -> notification"
-		if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
-		then
-			echo "${dir} is older than 14 days and will be deleted"
-			rm -rf "${dir}"
-		elif [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 7 ]]
-		then
-			echo "${dir} is older than 7 days, notification will be send"
-                        dirdate=$(date -r "${dir}")
-                        delete_date=$(date -d "${dirdate} +14 days")
-
-			#
-			# Compile JSON message payload.
-			#
-			read -r -d '' message << EOM
+                echo "There is Data in ${dir}, check if .finished file is present and if there's other data"
+                if [[ "${dir}/"*".finished" ]]
+                then
+			if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
+                	then
+                        	echo "${dir} is older than 14 days and will be deleted"
+                        	rm -rf "${dir}"
+                	fi
+		else
+                	if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
+                	then    
+                        	echo "${dir} is older than 14 days and will be deleted"
+                        	rm -rf "${dir}"
+                	elif [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 7 ]]
+                	then
+                        	echo "${dir} is older than 7 days, notification will be send"
+                        	dirdate=$(date -r "${dir}")
+                        	delete_date=$(date -d "${dirdate} +14 days")
+                
+                        	#
+                        	# Compile JSON message payload.
+                        	#
+                        	read -r -d '' message << EOM
 {
-	"type": "mrkdwn",
-	"text": "*Cleanup alert on _{{ slurm_cluster_name | capitalize }}_*:  
+        "type": "mrkdwn",
+        "text": "*Cleanup alert on _{{ slurm_cluster_name | capitalize }}_*:
 \`\`\`
-The following data on $(hostname) of the {{ slurm_cluster_name | capitalize }} cluster is older than a week: ${dir}. This 
+The following data on $(hostname) of the {{ slurm_cluster_name | capitalize }} cluster is older than a week: ${dir}. This
 data will be deleted on ${delete_date}!
 \`\`\`"
 }
 EOM
-
+                        
 #
 # Post message to Slack channel.
-#
+#               
 curl -X POST '{{ slurm_notification_slack_webhook }}' \
-	 -H 'Content-Type: application/json' \
-	 -d "${message}"
-					
-		else
-			echo "${dir} is not older than 7 days"
-		fi
+         -H 'Content-Type: application/json' \
+         -d "${message}" 
+
+                fi
 	fi
 done
