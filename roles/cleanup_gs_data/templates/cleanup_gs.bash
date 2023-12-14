3b2e73c2e2a7457b39fc5914d2c087a3ae57a8ad
marieke-bijlsma <mariekebijlsma01@gmail.com>
2023-12-14 11:17:50 +0100

    Count number of files and check .finished file

--- roles/cleanup_gs_data/templates/cleanup_gs.bash
+++ roles/cleanup_gs_data/templates/cleanup_gs.bash
@@ -1,7 +1,6 @@
 # If folder is empty for 2 weeks -> delete folder
 # If folder is not empty and no .finished file is present (1 week) -> notification
 # If folder is not empty and .finished file is present (2 weeks) -> delete folder
-
 #!/bin/bash
 
 dirToCheck="/groups/umcg-genomescan/"*
@@ -14,51 +13,52 @@ do
 	then
 		echo "${dir} is empty, check if it's older than 2 week -> delete"
 		if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
-                then
-                        echo "${dir} is older than 14 days and will be deleted"
-                        rm -rf "${dir}"
+		then
+			echo "${dir} is older than 14 days and will be deleted"
+			rm -rf "${dir}"
 		fi
 	else
-                echo "There is Data in ${dir}, check if .finished file is present and if there's other data"
-                if [[ "${dir}/"*".finished" ]]
-                then
+		echo "There is Data in ${dir}, check if .finished file is present and if there's other data"
+		numberOfFiles=$(find "${dir}" -mindepth 1 -maxdepth 1 -type f | wc -l)
+		if [[ ${numberOfFiles} == 1 && $(find "${dir}" -mindepth 1 -maxdepth 1 -type f) == *".finished" ]]
+		then
 			if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
-                	then
-                        	echo "${dir} is older than 14 days and will be deleted"
-                        	rm -rf "${dir}"
-                	fi
+			then
+				echo "${dir} is older than 14 days and will be deleted"
+				rm -rf "${dir}"
+			fi
 		else
-                	if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
-                	then    
-                        	echo "${dir} is older than 14 days and will be deleted"
-                        	rm -rf "${dir}"
-                	elif [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 7 ]]
-                	then
-                        	echo "${dir} is older than 7 days, notification will be send"
-                        	dirdate=$(date -r "${dir}")
-                        	delete_date=$(date -d "${dirdate} +14 days")
-                
-                        	#
-                        	# Compile JSON message payload.
-                        	#
-                        	read -r -d '' message << EOM
+			if [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 14 ]]
+			then    
+				echo "${dir} is older than 14 days and will be deleted"
+				rm -rf "${dir}"
+			elif [[ $(((${dateInSecNow} - $(date -r "${dir}" +%s)) / 86400)) -gt 7 ]]
+			then
+				echo "${dir} is older than 7 days, notification will be send"
+				dirdate=$(date -r "${dir}")
+				delete_date=$(date -d "${dirdate} +14 days")
+
+				#
+				# Compile JSON message payload.
+				#
+				read -r -d '' message << EOM
 {
-        "type": "mrkdwn",
-        "text": "*Cleanup alert on _{{ slurm_cluster_name | capitalize }}_*:
+	"type": "mrkdwn",
+	"text": "*Cleanup alert on _{{ slurm_cluster_name | capitalize }}_*:
 \`\`\`
 The following data on $(hostname) of the {{ slurm_cluster_name | capitalize }} cluster is older than a week: ${dir}. This
 data will be deleted on ${delete_date}!
 \`\`\`"
 }
 EOM
-                        
+
 #
 # Post message to Slack channel.
 #               
 curl -X POST '{{ slurm_notification_slack_webhook }}' \
-         -H 'Content-Type: application/json' \
-         -d "${message}" 
+	-H 'Content-Type: application/json' \
+	-d "${message}" 
 
-                fi
+		fi
 	fi
 done
